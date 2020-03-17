359193b3d80729cf7b9541c4c9f3703c656fcd61
ouzture <ouzture@gmail.com>
2020-03-17 18:47:24 +0300

    Added model POJOs. Closes #2

---
+++ src/main/java/ouzture/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package ouzture.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/ouzture/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package ouzture.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
