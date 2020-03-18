e75a71bbcc3c70c320cc840174a86129b1eb72c8
ouzture <ouzture@gmail.com>
2020-03-18 14:44:28 +0300

    Implement interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/ouzture/springframework/sfgpetclinic/services/PetService.java
@@ -0,0 +1,16 @@
+package ouzture.springframework.sfgpetclinic.services;
+
+import ouzture.springframework.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+
+}
