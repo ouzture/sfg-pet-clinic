c23da88dac87816a1809ccf5e1a1eea7641bab84
ouzture <ouzture@gmail.com>
2020-03-19 11:55:49 +0300

    Refactoring to common CrudService. Closes # 11

--- pet-clinic-data/src/main/java/ouzture/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/ouzture/springframework/sfgpetclinic/services/PetService.java
@@ -2,15 +2,8 @@ package ouzture.springframework.sfgpetclinic.services;
 
 import ouzture.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
+public interface PetService extends  CrudService<Pet,Long>{
 
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 
 }
