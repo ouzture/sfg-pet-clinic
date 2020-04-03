6d4a543db16935c960d9074141adde9884d2e6a1
ouzture <ouzture@gmail.com>
2020-04-03 14:35:13 +0300

    Added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/ouzture/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package ouzture.springframework.sfgpetclinic.services;
+
+import ouzture.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
