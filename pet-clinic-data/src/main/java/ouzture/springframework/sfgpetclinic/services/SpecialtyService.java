546f34c657c190cf17b6ef7e62099fc82dda9a78
ouzture <ouzture@gmail.com>
2020-04-05 15:34:21 +0300

    Added Specialties Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/ouzture/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package ouzture.springframework.sfgpetclinic.services;
+
+import ouzture.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty,Long> {
+}
