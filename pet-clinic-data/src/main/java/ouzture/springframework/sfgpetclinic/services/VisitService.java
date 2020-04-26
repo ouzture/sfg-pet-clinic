a785b28402b36a4a3abf4166728397b590440d25
ouzture <ouzture@gmail.com>
2020-04-26 10:12:04 +0300

    Implemented map implementation for Visit. Closes #46

---
+++ pet-clinic-data/src/main/java/ouzture/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package ouzture.springframework.sfgpetclinic.services;
+
+import ouzture.springframework.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long>{
+}
